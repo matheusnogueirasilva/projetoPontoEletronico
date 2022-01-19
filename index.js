88320b0f5e1b1ff45adeb450bac87fcf351f685c
Matheus Nogueira Silva <52356770+matheusnogueirasilva@users.noreply.github.com>
2022-01-19 16:45:58 -0300

    Transformando em matriz

--- index.js
+++ index.js
@@ -2,7 +2,19 @@
 
 let fs = require('fs');
 
+
+
 fs.readFile('./PontoDiario.csv', 'utf-8', function (err, data) {
+    let lista = [];
+    let matriz = [];
+
     if(err) throw err;
-    console.log(data);
+
+    lista = data.split('\n');
+    
+    for(i = 0; i < lista.length; i++){
+        matriz.push(lista[i].split(';'));
+    }
+
+    console.log(matriz);
 });
